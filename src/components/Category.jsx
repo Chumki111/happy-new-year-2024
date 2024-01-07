45dd5d09ef2ef5897dd079250dd1a0a7ba70f45e
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-08 00:37:12 +0600

    category section

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,12 @@
-
+import category from '../assets/category.png'
 
 const Category = () => {
     return (
-        <div>
-            category
+        <div className="text-center mt-16">
+            <h1 className="text-6xl text-[#F00] pb-10 font-bold">65% OFF</h1>
+            <p className="text-[#F00] font-medium py-3">OUR HOLIDAY DEALS ARE BETTER</p>
+            <h2 className="text-4xl font-bold"> ALL CATEGORIES</h2>
+            <img src={category} alt="" className='mx-auto my-10'/>
         </div>
     );
 };
